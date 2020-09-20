05c2f60917206cf40263a145e2aef1e9c85a39f9
Christian Valdes <christianv042393@gmail.com>
2020-09-20 19:40:03 -0400

    added conditionally rendered log

--- js/app.js
+++ js/app.js
@@ -3,19 +3,25 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        runningGame: false
+        runningGame: false,
+        turns: []
     },
     methods: {
         startGame: function() {
             this.runningGame = true;
             this.playerHealth = 100;
-            this.monsterHealth = 100
+            this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function() {
             var max = 10;
             var min = 2;
             var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
             this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            });
 
             if (this.monsterHealth <= 0) {
                 alert('You won!');
@@ -27,6 +33,10 @@ new Vue({
             var min = 4;
             var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
             this.playerHealth -= damage;
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits player for ' + damage
+            });
 
             if (this.playerHealth <= 0) {
                 alert('You lost!');
@@ -38,6 +48,10 @@ new Vue({
             var min = 10;
             var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
             this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster hard for ' + damage
+            });
 
             if (this.monsterHealth <= 0) {
                 alert('You won!');
@@ -60,6 +74,14 @@ new Vue({
             var min = 2;
             var healing = Math.max(Math.floor(Math.random() * max) + 1, min);
             this.playerHealth += healing;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals up' 
+            });
+
+            if (this.playerHealth <= 100) {
+                !this.turns.unshift();
+            }
 
             if (this.playerHealth <= 90) {
                 this.playerHealth +- healing;
